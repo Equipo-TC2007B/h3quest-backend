72a532848179d149764ba9ae0e4b2f95e9ed2eb6
EmilianoCBE <e.carrizales1305@gmail.com>
2026-04-28 15:58:12 -0600

    Creación de rutas para mod de talleres

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const userRoutes = require("./routes/userRoutes");
 const questRoutes = require("./routes/questRoutes");
 const preguntasRoutes = require("./routes/preguntasRoutes");
 const adminRoutes = require("./routes/adminRoutes");
+const tallerRoutes = require("./routes/tallerRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -19,6 +20,7 @@ app.use("/api/users", userRoutes);
 app.use("/api/quests", questRoutes);
 app.use("/api/preguntas", preguntasRoutes);
 app.use("/api/admin", adminRoutes);
+app.use("/api/talleres", tallerRoutes);
 
 // Ruta de prueba base
 app.get("/", (req, res) => {
