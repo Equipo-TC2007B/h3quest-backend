6d1ae3c052353f2856daeac76e7cbd89517a8045
EmilianoCBE <e.carrizales1305@gmail.com>
2026-04-28 19:39:13 -0600

    Agregando token en questRoutes

--- routes/questRoutes.js
+++ routes/questRoutes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const questController = require("../controllers/questController");
 
+const verificarToken = require("../middlewares/authMiddleware");
+
 // ==========================
 // RUTAS PÚBLICAS (O PRIVADAS SI REQUIEREN TOKEN)
 // ==========================
